93cb4eaeabb638a21f00dd19fa1b35fe189c675f
JuanDuran85 <duran85juan@gmail.com>
2020-03-08 22:06:26 -0300

    modificando ejemplo3

--- ejercicio_3/components/directiveIf.js
+++ ejercicio_3/components/directiveIf.js
@@ -4,9 +4,9 @@ let directivaIf = {
             <hr>
             <h2 v-text="titulo"></h2>
             <p v-html="mensaje"></p>
-            <p v-if="mostrar">Mostrando y Ocultando con la directiva v-show</p>
+            <p v-if="mostrar">Mostrando y Ocultando con la directiva v-show/v-if</p>
             <p v-if="usuario.permiso">El usuario tiene permiso para ver la información</p>
-            <p v-if="!usuario.permiso">El usuario no tiene permiso para ver la información</p>
+            <p v-else="usuario.permiso">El usuario no tiene permiso para ver la información</p>
         </div>
     `,
     data() {
