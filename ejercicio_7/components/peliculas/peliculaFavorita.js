435e6413b2f31f780e1a7916bf925705454c9816
JuanDuran85 <duran85juan@gmail.com>
2020-03-14 18:43:52 -0300

    modificando ejercicio 7

--- ejercicio_7/components/peliculas/peliculaFavorita.js
+++ ejercicio_7/components/peliculas/peliculaFavorita.js
@@ -8,4 +8,28 @@ let peliculaFavorita = {
             </div>
         </div>
     `,
+    beforeCreate() {
+        console.log("beforeCreate desde peliculaFavorita");
+    },
+    created() {
+        console.log("Create desde peliculaFavorita");
+    },
+    beforeMount() {
+        console.log("beforeMount desde peliculaFavorita");
+    },
+    mounted() {
+        console.log("Mounted desde peliculaFavorita");
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate desde peliculaFavorita");
+    },
+    updated() {
+        console.log("Updated desde peliculaFavorita");
+    },
+    beforeDestroy() {
+        console.log("beforeDestroy desde peliculaFavorita");
+    },
+    destroyed() {
+        console.log("Destroyed desde peliculaFavorita");
+    },
 }
