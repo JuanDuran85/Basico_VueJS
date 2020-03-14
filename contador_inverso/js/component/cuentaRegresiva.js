63657ccb41e72562cfc7ba6a7dbd659ecb58db17
JuanDuran85 <duran85juan@gmail.com>
2020-03-14 00:10:03 -0300

    cambios a posible solucion de cuenta regresiva

--- contador_inverso/js/component/cuentaRegresiva.js
+++ contador_inverso/js/component/cuentaRegresiva.js
@@ -3,12 +3,16 @@ Vue.component('cuenta-regresiva',{
         <div class="p-3 m-3">
             <p v-text="cuenta"></p>
             <button @click="tresSeg">3seg</button>
+            <button @click="cincoSeg">5seg</button>
+            <button @click="unMin">1min</button>
         </div>
     `,
     data() {
         return {
-            cuenta: "00:00:00",
+            cuenta: "00:00:00 H:min:seg",
             tres: 3,
+            cinco: 5,
+            uno: 60
         }
     },
     methods: {
@@ -22,6 +26,32 @@ Vue.component('cuenta-regresiva',{
                 }else {
                     this.tres = 3;
                     clearInterval(cuentaTres);
+                }
+            },1000);
+        },
+        cincoSeg(){
+            this.cuenta = `00:00:05seg`;
+            let cuentaCinco = setInterval(()=>{
+                if (this.cinco > 0) {
+                    console.log(this.cinco);
+                    this.cinco--;
+                    this.cuenta = `00:00:0${this.cinco}seg`
+                }else {
+                    this.cinco = 5;
+                    clearInterval(cuentaCinco);
+                }
+            },1000);
+        },
+        unMin(){
+            this.cuenta = `00:01:00 min-seg`;
+            let cuentaUno = setInterval(()=>{
+                if (this.uno > 0) {
+                    console.log(this.uno);
+                    this.uno--;
+                    this.cuenta = `00:00:${this.uno}seg`
+                }else {
+                    this.uno = 60;
+                    clearInterval(cuentaUno);
                 }
             },1000);
         }
