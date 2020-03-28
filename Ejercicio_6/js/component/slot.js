889ad5546fb3b30cf81b6116d341433c047a2df2
JuanDuran85 <duran85juan@gmail.com>
2020-03-27 22:03:53 -0300

    final ejercicio 6 slot

--- Ejercicio_6/js/component/slot.js
+++ Ejercicio_6/js/component/slot.js
@@ -15,8 +15,7 @@ Vue.component("ventana-modal", {
                 <slot></slot>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <slot name="footer"></slot>
               </div>
             </div>
           </div>
