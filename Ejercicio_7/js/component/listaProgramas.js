31a500306dd266f1b8c080af8b40131ebd5cb453
JuanDuran85 <duran85juan@gmail.com>
2020-03-29 23:04:55 -0300

    modificando ejercicio 7

--- Ejercicio_7/js/component/listaProgramas.js
+++ Ejercicio_7/js/component/listaProgramas.js
@@ -20,20 +20,20 @@ const ListaProgramas = {
             <hr>
             <label></label>
             <div class="form-group">
-              <label>Ingresa un Nombre de Ciudad</label>
+              <label>Ingresa un Nombre de Ciudad o país</label>
               <input type="text"
-                class="form-control" v-model="buscarNombre" aria-describedby="helpId" placeholder="Ingresa un nombre de ciudad">
-              <small id="helpId" class="form-text text-muted">Ingresa un nombre de ciudad</small>
+                class="form-control" v-model="buscarNombre" aria-describedby="helpId" placeholder="Ingresa un nombre de ciudad o país">
+              <small id="helpId" class="form-text text-muted">Ingresa un nombre de ciudad o país</small>
             </div>
             <div class="btn-group" data-toggle="buttons">
                 <label class="btn btn-primary">Activo
-                    <input type="checkbox" autocomplete="off" v-model="activo">
+                    <input type="checkbox" autocomplete="off" v-model="activo" name="op1">
                 </label>
             </div>
             <div class="btn-group" data-toggle="buttons">
-            <label class="btn btn-primary">Inactivo
-                <input type="checkbox" autocomplete="off" v-model="inactivo">
-            </label>
+                <label class="btn btn-primary">Inactivo
+                    <input type="checkbox" autocomplete="off" v-model="inactivo" name="op1">
+                </label>
         </div>
             <table class="table table-hover">
                 <thead>
@@ -46,7 +46,7 @@ const ListaProgramas = {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(lugar,index) in paises2" :key="index" v-if="filtrarNombre(lugar)">
+                    <tr v-for="(lugar,index) in paises2" :key="index" v-show="filtrarNombre(lugar)">
                         <th>{{index+1}}</th>
                         <td>{{lugar.pais}}</td>
                         <td>{{lugar.capital}}</td>
@@ -61,20 +61,20 @@ const ListaProgramas = {
         return {
             programas: ["C++","PHP","Java","JavaScript","C#"],
             paises: [
-                {pais:"Venezuela",capital:"Caracas",poblacion:28067000},
-                {pais:"Chile",capital:"Santiago de Chile",poblacion:19107216},
-                {pais:"Peru",capital:"Lima",poblacion:33105273},
-                {pais:"Argentina",capital:"Buenos Aires",poblacion:44938712},
-                {pais:"Colombia",capital:"Bogota",poblacion:50372424}
+                {pais:"Venezuela",capital:"Caracas",poblacion:"28067000"},
+                {pais:"Chile",capital:"Santiago de Chile",poblacion:"19107216"},
+                {pais:"Peru",capital:"Lima",poblacion:"33105273"},
+                {pais:"Argentina",capital:"Buenos Aires",poblacion:"44938712"},
+                {pais:"Colombia",capital:"Bogota",poblacion:"50372424"}
             ],
             paises2: [
-                {pais:"Venezuela",capital:"Caracas",poblacion:28067000, activo: true},
-                {pais:"Chile",capital:"Santiago de Chile",poblacion:19107216, activo: true},
-                {pais:"Peru",capital:"Lima",poblacion:33105273, activo: false},
-                {pais:"Argentina",capital:"Buenos Aires",poblacion:44938712, activo: true},
-                {pais:"Colombia",capital:"Bogota",poblacion:50372424, activo: false}
+                {pais:"Venezuela",capital:"Caracas",poblacion:"28067000", activo: true},
+                {pais:"Chile",capital:"Santiago de Chile",poblacion:"19107216", activo: true},
+                {pais:"Peru",capital:"Lima",poblacion:"33105273", activo: false},
+                {pais:"Argentina",capital:"Buenos Aires",poblacion:"44938712", activo: true},
+                {pais:"Colombia",capital:"Bogota",poblacion:"50372424", activo: false}
             ],
-            filtro_poblacion: 10000000,
+            filtro_poblacion: "10000000",
             buscarNombre: '',
             CopiaPaises: [],
             inactivo: false,
@@ -89,13 +89,15 @@ const ListaProgramas = {
             })
         },
         filtrarNombre(lugar){
-            if (this.activo) {
-                let guarda = lugar.pais.toLowerCase().indexOf(this.buscarNombre.toLowerCase()) >= 0;
-                return (lugar.activo && this.activo && guarda);
-            } else if (this.inactivo){
-                return (!lugar.activo && this.inactivo);
-            }else {
-                return lugar.pais.toLowerCase().indexOf(this.buscarNombre.toLowerCase()) >= 0
+            let porCiudad, porPais;
+            porPais = lugar.pais.toLowerCase().indexOf(this.buscarNombre.toLowerCase()) >= 0;
+            porCiudad = lugar.capital.toLowerCase().indexOf(this.buscarNombre.toLowerCase()) >= 0;
+            if (this.activo && !this.inactivo) {
+                return (lugar.activo && this.activo && (porCiudad || porPais));
+            } else if (this.inactivo && !this.activo){
+                return (!lugar.activo && this.inactivo && (porCiudad || porPais));
+            } else {
+                return (porCiudad || porPais);
             }
         },
     },
