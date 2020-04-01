d6c49f41e0cc27f3f3324536d4cb73ac7b90bb4e
JuanDuran85 <duran85juan@gmail.com>
2020-03-31 23:24:57 -0300

    agregando Ejemplo_8 Vue-Webpack desde cero

---
+++ Ejemplo_8/src/index.js
@@ -0,0 +1,6 @@
+import componente from './components/componente';
+
+const app = new Vue({
+    el: "#app",
+    render: h => h(componente)
+})
---
+++ Ejemplo_8/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //tanto los nombres de entrada y salida por defecto, se pueden mmodificar agregando el archivo de configuracion de webpack y modificando manualmente cada uno de los parametros
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js'      
+    }
+};
